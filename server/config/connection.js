755307e29b8aa7c39b70f772ab8c0ecf740c9230
Taster101 <Davontahenderson1@gmail.com>
2023-06-13 14:53:31 -0500

    push to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
+ls8UPlEzKsmJWG7V
+mongodb+srv://papabear23:ls8UPlEzKsmJWG7V@cluster0.fwjg4ox.mongodb.net/
